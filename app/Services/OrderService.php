23b068426205f2f964bb9c25e5da91da4228b7aa
oomusou <oomusou@gmail.com>
2016-06-18 22:17:18 +0800

    建立 OrderService

---
+++ app/Services/OrderService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+class OrderService
+{
+    /**
+     * 計算今天全部訂單金額
+     * @return int
+     */
+    public function calculateTodayTotalAmount() : int
+    {
+        return 1300;
+    }
+
+}
