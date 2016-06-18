a74e7cc1327e79f0a47e967d225cbaf5c88197f5
oomusou <oomusou@gmail.com>
2016-06-18 22:00:03 +0800

    [單元測試] 今天全部訂單金額

--- tests/Unit/OrderServiceTest.php
+++ tests/Unit/OrderServiceTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Order;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class OrderServiceTest extends TestCase
@@ -18,6 +19,39 @@ class OrderServiceTest extends TestCase
 
         /** assert */
         $this->assertCount($expected, $actual);
+    }
+
+    /** @test */
+    public function 今天全部訂單金額()
+    {
+        /** Arrange */
+        Carbon::setTestNow(Carbon::create(2016, 6, 18));
+
+        Order::create([
+            'order_date' => Carbon::create(2016, 6, 17),
+            'quantity'   => 1,
+            'price'      => 100
+        ]);
+
+        Order::create([
+            'order_date' => Carbon::create(2016, 6, 18),
+            'quantity'   => 2,
+            'price'      => 200
+        ]);
+
+        Order::create([
+            'order_date' => Carbon::create(2016, 6, 18),
+            'quantity'   => 3,
+            'price'      => 300
+        ]);
+
+        $expected = 1300;
+
+        /** Act */
+        $actual = app(OrderService::class)->calculateTodayTotalAmount();
+
+        /** Assert */
+        $this->assertEquals($expected, $actual);
     }
 }
 
