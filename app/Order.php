334cafb559407c52d4fac41fb6cce59e371da8f8
oomusou <oomusou@gmail.com>
2016-06-18 22:35:54 +0800

    建立 Order model 註解

--- app/Order.php
+++ app/Order.php
@@ -4,6 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Order
+ *
+ * @mixin \Eloquent
+ * @property integer $id
+ * @property string $order_date
+ * @property integer $quantity
+ * @property integer $price
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Order whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Order whereOrderDate($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Order whereQuantity($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Order wherePrice($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Order whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Order whereUpdatedAt($value)
+ */
 class Order extends Model
 {
     protected $fillable = [
