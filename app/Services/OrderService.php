e5112c689f60ceded162e52ac4dd91a783c42a6c
oomusou <oomusou@gmail.com>
2016-06-19 11:30:59 +0800

    建立 foreach 版本 OrderService

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -2,15 +2,38 @@
 
 namespace App\Services;
 
+use App\Order;
+use Carbon\Carbon;
+
 class OrderService
 {
+    /** @var Order */
+    private $order;
+
+    /**
+     * OrderService constructor.
+     * @param Order $order
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
     /**
      * 計算今天全部訂單金額
      * @return int
      */
     public function calculateTodayTotalAmount() : int
     {
-        return 1300;
-    }
+        $totalAmount = 0;
 
+        $orders = $this->order->all();
+        foreach($orders as $order) {
+            if ($order->order_date == Carbon::now()) {
+                $totalAmount = $totalAmount + $order->quantity * $order->price;
+            }
+        }
+
+        return $totalAmount;
+    }
 }
