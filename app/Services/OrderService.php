38bc52878707f137f5ce87a41ad9c3fd4b90057d
oomusou <oomusou@gmail.com>
2016-06-19 13:55:44 +0800

    將 Closure 加以重構

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Order;
 use Carbon\Carbon;
+use Closure;
 
 class OrderService
 {
@@ -26,14 +27,41 @@ class OrderService
     public function calculateTodayTotalAmount() : int
     {
         return $this->order->all()
-            ->filter(function ($value) {
-                return $value->order_date == Carbon::now();
-            })
-            ->map(function ($value) {
-                return $value->quantity * $value->price;
-            })
-            ->reduce(function ($carry, $value) {
-                return $carry + $value;
-            });
+            ->filter($this->filterToToday())
+            ->map($this->mapToAmount())
+            ->reduce($this->reduceToTotalAmount());
+    }
+
+    /**
+     * 只有今天的訂單
+     * @return Closure
+     */
+    private function filterToToday()
+    {
+        return function ($value) {
+            return $value->order_date == Carbon::now();
+        };
+    }
+
+    /**
+     * 換算成金額
+     * @return Closure
+     */
+    private function mapToAmount()
+    {
+        return function ($value) {
+            return $value->quantity * $value->price;
+        };
+    }
+
+    /**
+     * 換算成總金額
+     * @return Closure
+     */
+    private function reduceToTotalAmount()
+    {
+        return function ($carry, $value) {
+            return $carry + $value;
+        };
     }
 }
